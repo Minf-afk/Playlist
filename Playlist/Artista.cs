2698874c4bf5a6af9bdcec7acfb85224fa2fbce3
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-12 12:38:08 -0300

    Implementacao do ctor de Artista

--- Playlist/Artista.cs
+++ Playlist/Artista.cs
@@ -6,6 +6,15 @@ namespace Playlist
 {
     class Artista
     {
+        public Artista(string nome, int idade, DateTime nasc, int rg, int cpf, string endereco)
+        {
+            NomeCompleto = nome;
+            Idade = idade;
+            DataDeNascimento = nasc;
+            Rg = rg;
+            Cpf = cpf;  
+            Endereco = endereco;
+        }
         public string NomeCompleto { get; set; }
         public int Idade { get; set; }
         public DateTime DataDeNascimento { get; set; }
