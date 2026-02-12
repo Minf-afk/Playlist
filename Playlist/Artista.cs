111a19059dee21134b54dc3fb6617ce0fb7dda53
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-12 12:36:26 -0300

    Implementacao de prop Artista

--- Playlist/Artista.cs
+++ Playlist/Artista.cs
@@ -6,6 +6,13 @@ namespace Playlist
 {
     class Artista
     {
+        public string NomeCompleto { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public int Rg { get; set; }
 
+        public int Cpf { get; set; }
+
+        public string Endereco { get; set; }
     }
 }
