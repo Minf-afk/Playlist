7904ffcb191f7c7b6844439ad3af70118bcfa4b6
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-13 16:58:15 -0300

    Criacao do Cntrs

--- Playlist/AvaliacaoMusical.cs
+++ Playlist/AvaliacaoMusical.cs
@@ -6,6 +6,13 @@ namespace Playlist
 {
     class AvaliacaoMusical
     {
+        public AvaliacaoMusical(string avaliador, int nota, DateTime data, string descricao)
+        {
+            UsuarioAvaliador = avaliador;
+            NotaAvaliadora = nota;
+            DataDaAvaliacao = data;
+            DescricaoAvaliacao = descricao;
+        }
         public string UsuarioAvaliador { get; set; }
         public int NotaAvaliadora { get; set; }
         public DateTime DataDaAvaliacao { get; set; }
