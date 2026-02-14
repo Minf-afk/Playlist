d13f76a158781aff7d1b24acf3cdea7096072448
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-14 09:32:15 -0300

    prop da Busca

--- Playlist/Busca.cs
+++ Playlist/Busca.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Playlist
 {
-    internal class Busca
+    public class Busca
     {
+        public int index { get;}
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
     }
 }
